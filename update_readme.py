9206a355a59235c18130c3f9a0337020d77bda8a
Junseok Oh <ixtears@naver.com>
2023-09-27 16:28:49 +0900

    README 자동 생성 시 날짜 추가

--- update_readme.py
+++ update_readme.py
@@ -1,4 +1,22 @@
 import os
+import subprocess
+from datetime import datetime
+
+def get_git_creation_date(file_path):
+    try:
+        # git log 명령을 사용하여 파일의 최초 커밋 날짜를 가져옵니다.
+        date_str = subprocess.check_output(
+            ['git', 'log', '--diff-filter=A', '--', file_path],
+            universal_newlines=True,
+        ).split('\n')[2].split('   ')[1]
+
+        # 날짜 문자열을 datetime 객체로 변환합니다.
+        date = datetime.strptime(date_str, '%c %z')
+        return date.strftime('%Y-%m-%d')
+    except Exception as e:
+        # git log 명령이 실패하면 None을 반환합니다.
+        print(f"Error getting creation date for {file_path}: {e}")
+        return None
 
 # README 파일을 생성할 디렉토리를 지정합니다.
 project_dir = '.'
@@ -28,7 +46,13 @@ with open('README.md', 'w') as readme_file:
             if filename != 'README.md' and not filename.lower().endswith('.gif'):
                 clean_path = os.path.join(foldername, filename).replace('./', '', 1)  # "./" 제거
                 url_encoded_path = clean_path.replace(' ', '%20')  # 띄어쓰기를 %20으로 변환
-                readme_file.write(f'- [{filename}]({url_encoded_path})\n')
+
+                # git 최초 등록 시간을 가져옵니다.
+                date_str = get_git_creation_date(clean_path)
+                if date_str:
+                    readme_file.write(f'- [{filename}]({url_encoded_path}) ({date_str})\n')
+                else:
+                    readme_file.write(f'- [{filename}]({url_encoded_path})\n')
 
         # 서브 폴더 리스트가 비어 있지 않으면 추가 공백 라인을 삽입합니다.
         if subfolders:
